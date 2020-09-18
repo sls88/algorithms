fe14008a05cf1d5ef00f3681f8a36f6def487b19
Dmitry Z <sls8@ya.ru>
2020-09-18 18:21:53 +0300

    binary_search

---
+++ binary_search.py
@@ -0,0 +1,19 @@
+def binary_search(lst, item):
+    low = 0
+    high = len(lst) - 1
+
+    while low <= high:
+        mid = int((low + high) / 2)
+        guess = lst[mid]
+        if guess == item:
+            return mid
+        elif guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+my_list = [1, 3, 5, 7, 9]
+
+print(binary_search(my_list, 7))
+print(binary_search(my_list, 10))
