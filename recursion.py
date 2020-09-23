7e51c5343d21daa0bc5060650a063cf5646b3af7
Dmitry Z <sls8@ya.ru>
2020-09-23 18:22:52 +0300

    Рекурсия. Сумма чисел

--- recursion.py
+++ recursion.py
@@ -14,3 +14,11 @@ def fact(x):
         return x * fact(x-1)
 
 print(fact(5))
+
+def sum2(arr):
+    if len(arr) == 1:
+        return arr[0]
+    else:
+        return arr[0] + sum2(arr[1:])
+
+print(sum2([1, 2, 3, 4, 5]))
